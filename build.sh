f1da7526f5c845586bcff5f194ec3aab9d7fc03e
LoveDoLove <v0130p5100cuboss@gmail.com>
2025-05-12 21:03:10 +0800

    Release 1.0.1

--- build.sh
+++ build.sh
@@ -9,12 +9,23 @@ version=$(git describe --long --tags --dirty --always)
 
 ldflags="\
 -w -s \
--X 'github.com/cloudreve/Cloudreve/v3/pkg/conf.BuiltAt=$builtAt' \
--X 'github.com/cloudreve/Cloudreve/v3/pkg/conf.GoVersion=$goVersion' \
--X 'github.com/cloudreve/Cloudreve/v3/pkg/conf.GitAuthor=$gitAuthor' \
--X 'github.com/cloudreve/Cloudreve/v3/pkg/conf.LastCommit=$gitCommit' \
--X 'github.com/cloudreve/Cloudreve/v3/pkg/conf.BackendVersion=$version' \
+-X 'github.com/cloudreve/Cloudreve/v4/pkg/conf.BuiltAt=$builtAt' \
+-X 'github.com/cloudreve/Cloudreve/v4/pkg/conf.GoVersion=$goVersion' \
+-X 'github.com/cloudreve/Cloudreve/v4/pkg/conf.GitAuthor=$gitAuthor' \
+-X 'github.com/cloudreve/Cloudreve/v4/pkg/conf.LastCommit=$gitCommit' \
+-X 'github.com/cloudreve/Cloudreve/v4/pkg/conf.BackendVersion=$version' \
 "
+
+# Ensure assets.zip is in the correct location for go:embed
+if [ ! -f application/statics/assets.zip ]; then
+  if [ -f assets.zip ]; then
+    mkdir -p application/statics
+    cp assets.zip application/statics/
+  else
+    echo "Error: assets.zip not found in project root."
+    exit 1
+  fi
+fi
 
 export CGO_ENABLED=1
 export CC="clang --target=x86_64-unknown-freebsd14.1 --sysroot=/opt/freebsd"
